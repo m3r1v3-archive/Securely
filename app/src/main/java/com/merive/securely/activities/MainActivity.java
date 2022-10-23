eb5fba22da37eb3afb078eaddf39f92062529cb2
Max Meriberg <merive.official@gmail.com>
2022-10-23 14:20:48 +0300

    feat: update layout files
    - Refactor MainActivity

--- app/src/main/java/com/merive/securely/activities/MainActivity.java
+++ app/src/main/java/com/merive/securely/activities/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
